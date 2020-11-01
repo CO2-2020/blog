213710d746694c864a2aa700cfe614486165536f
CO2-2020 <tushan4872@163.com>
2020-11-01 18:52:28 +0800

    fix(html): fix page visibility problem

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 记录个人博客、读书笔记等，欢迎star。
 # Vue
 [Vue2.x和Vue3.x dom-diff 对比](https://co2-2020.github.io/blog/soundcode/vue/dom-diff.html)
+# HTML
+[Page Visibility API 概念及使用场景](https://co2-2020.github.io/blog/soadancedJsundcode/html/Page-Visibility.html)
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -78,6 +78,15 @@ module.exports = {
             ],
             '/adancedJs/': [
                 {
+                    title: 'HTML',   // 必要的
+                    path: '/adancedJs/html/Page-Visibility',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+                    collapsable: true, // 可选的, 默认值是 true,
+                    sidebarDepth: 1,    // 可选的, 默认值是 1
+                    children: [
+                        'html/Page-Visibility',
+                    ]
+                },
+                {
                     title: 'JavaScript',   // 必要的
                     path: '/adancedJs/base/closure',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
                     collapsable: true, // 可选的, 默认值是 true,
